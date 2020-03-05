be6ce6db17644e4f063615e36728fc2b7570f501
Magombo <Uo258418@uniovi.es>
2020-03-05 11:41:51 +0100

    changed number of FPS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width = window.innerWidth;
 var height = ctx.canvas.height = window.innerHeight;
 
-const fps = 2;
+const fps = 10;
 const rows = 50, cols = 30;
 const cellSize = 20;
 const aliveCellsPc = 0.1;
